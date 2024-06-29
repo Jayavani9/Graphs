fdfc087a86b40f2e7a271e7bf065b79eb99a1610
Jayavani Akkiraju <51267675+Jayavani9@users.noreply.github.com>
2024-06-29 13:17:31 -0700

    Update DFSGraph.java

--- DFSGraph.java
+++ DFSGraph.java
@@ -26,3 +26,49 @@ class Solution {
         }
     }
 }
+
+
+
+
+//Using Stack
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+         boolean[] visited = new boolean[V];
+       
+        ArrayList<Integer> result = new ArrayList<>();
+   
+        Stack<Integer> stack = new Stack<>();
+        
+    
+        stack.push(0);
+        visited[0] = true;
+        result.add(0);  
+        
+        while (!stack.isEmpty()) {
+            int v = stack.peek();
+            boolean allVisited = true;
+            
+
+            for (int neighbor : adj.get(v)) {
+                if (!visited[neighbor]) {
+                    stack.push(neighbor);
+                    visited[neighbor] = true;
+                    result.add(neighbor);  
+                    allVisited = false;
+                    break;
+                }
+            }
+            
+    
+            if (allVisited) {
+                stack.pop();
+            }
+        }
+        
+        return result;
+    }
+  
+}
