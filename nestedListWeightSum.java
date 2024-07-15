be364e792a73714bd26f7a0eb9b7f16ebcf9299b
Jayavani Akkiraju <51267675+Jayavani9@users.noreply.github.com>
2024-07-15 12:48:07 -0700

    Create nestedListWeightSum.java

---
+++ nestedListWeightSum.java
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/nested-list-weight-sum/
+
+class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        //Tc: O(n) Sc: O(n)
+        Queue<NestedInteger> q = new LinkedList<>();
+        q.addAll(nestedList);
+        
+        int depth = 1;
+        int ans = 0;
+
+        while(!q.isEmpty())
+        {
+            int n = q.size();
+            for(int i = 0; i < n; i++)
+            {
+                NestedInteger cur = q.poll();
+                if(cur.isInteger())
+                {
+                    ans += cur.getInteger() * depth;
+                }
+
+                else 
+                {
+                    q.addAll(cur.getList());
+                }
+            }
+            depth++;
+        }
+        return ans;
+    }
+}
